fd324b8b1efaa34ad034d0c01cfd4246572029e7
1shaharear <sonarbangla.allah@gmail.com>
2021-12-25 23:03:01 +0600

    Laravel8Ecommerce

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//   return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class);
+
+Route::get('/Shop', ShopComponent::class);
+
+Route::get('/Cart', CartComponent::class);
+
+Route::get('/Checkout', CheckoutComponent::class);
